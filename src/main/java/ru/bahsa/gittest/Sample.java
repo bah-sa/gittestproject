55be7306fd37a8b4142ffe3a594662109a101cd9
bah-sa <bah.sa@yandex.ru>
2017-11-02 22:08:47 +0300

    Added second println

--- src/main/java/ru/bahsa/gittest/Sample.java
+++ src/main/java/ru/bahsa/gittest/Sample.java
@@ -3,5 +3,6 @@ package ru.bahsa.gittest;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
+        System.out.println("Hello, Branch1!");
     }
 }
