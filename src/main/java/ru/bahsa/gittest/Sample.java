cbe43d7ad9ca0f85ddad18640b561a08b874d66a
bah-sa <bah.sa@yandex.ru>
2017-11-03 07:59:34 +0300

    Added javadoc

--- src/main/java/ru/bahsa/gittest/Sample.java
+++ src/main/java/ru/bahsa/gittest/Sample.java
@@ -1,5 +1,8 @@
 package ru.bahsa.gittest;
 
+/**
+ * Доработали JavaDoc для файла
+ */
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
